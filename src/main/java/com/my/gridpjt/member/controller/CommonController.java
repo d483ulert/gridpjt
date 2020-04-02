70df61cd38118845ce564c44823a81bfb46ba10b
jisung324 <jisung3245@gmail.com>
2020-04-03 00:46:36 +0900

    spring security

---
+++ src/main/java/com/my/gridpjt/member/controller/CommonController.java
@@ -0,0 +1,24 @@
+package com.my.gridpjt.member.controller;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import lombok.extern.log4j.Log4j;
+
+
+@Log4j
+@Controller
+public class CommonController {
+	
+	@GetMapping("/accessError")
+	public void accessDenied(Authentication auth, Model model) {
+		
+		model.addAttribute("msg","Access Denied");
+		
+	}
+
+	
+	
+}
---
+++ src/main/webapp/WEB-INF/views/member/accessError.jsp
@@ -0,0 +1,15 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Access Denied</title>
+</head>
+<body>
+
+	<h1>Access Denied</h1>
+	<h2><c:out value="${SPRING_SECURITY_403_EXCEPTION.getMessage()} "/></h2>
+	<h2><c:out value="${msg}"></h2>
+</body>
+</html>
