65fae6076dffe4834b2c7fc0786d14b36365df04
jisung324 <jisung3245@gmail.com>
2020-06-25 21:01:13 +0900

    controller

--- src/main/java/com/my/gridpjt/boardcontroller/BoardController.java
+++ src/main/java/com/my/gridpjt/boardcontroller/BoardController.java
@@ -6,7 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
 import lombok.extern.log4j.Log4j;
 import lombok.AllArgsConstructor;
 
@@ -18,10 +22,45 @@ public class BoardController {
 
 	@Autowired
 	private BoardService service;
-	
+
+	//목록
 	@GetMapping("/list")
 	public void list(Model model)
 	{
 		model.addAttribute("list",service.getList());
+	}
+	
+	//등록
+	@PostMapping("/register")
+	public String register(BoardVO board, RedirectAttributes rttr) {
+		service.register(board);
+		rttr.addFlashAttribute("result",board.getBno());
+		
+		return "redirect:/board/list";		
+	}
+	
+	//읽기
+	@GetMapping("/get")
+	public void get(@RequestParam("bno") Long bno, Model model) {
+		model.addAttribute("board",service.get(bno));
+	}
+	
+	//수정
+	@PostMapping("/modify")
+	public String modify(BoardVO board, RedirectAttributes rttr) {
+		if(service.modify(board)){
+			rttr.addFlashAttribute("result","success");
+		}
+		return "redirect:/board/list";
+	}
+	
+	//삭제
+	@PostMapping("/remove")
+	public String remove(@RequestParam("bno") Long bno, RedirectAttributes rttr){
+		if(service.remove(bno)) {
+			rttr.addFlashAttribute("result","success");
+		}
+		
+		return "redirect:/board/list";
 	}
 }
