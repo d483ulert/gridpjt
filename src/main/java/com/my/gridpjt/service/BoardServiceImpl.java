11597807185f745a8356519a44c8eb98c9709544
jisung324 <jisung3245@gmail.com>
2020-06-28 00:00:40 +0900

    @autowired 에러수정

--- src/main/java/com/my/gridpjt/service/BoardServiceImpl.java
+++ src/main/java/com/my/gridpjt/service/BoardServiceImpl.java
@@ -16,7 +16,7 @@ import lombok.extern.log4j.Log4j;
 @Service
 public class BoardServiceImpl implements BoardService {
 
-	@Autowired
+	@Autowired(required=true)
 	private BoardMapper mapper;
 	
 	@Override
