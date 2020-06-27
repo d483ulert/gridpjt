a3e1dd2f10d38c5196b9e1ee69a976a0fbfea917
jisung324 <jisung3245@gmail.com>
2020-06-27 23:43:19 +0900

    encoding

--- src/main/java/com/my/gridpjt/service/BoardServiceImpl.java
+++ src/main/java/com/my/gridpjt/service/BoardServiceImpl.java
@@ -8,7 +8,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import lombok.AllArgsConstructor;
-import lombok.Setter;
 import lombok.extern.log4j.Log4j;
 
 @AllArgsConstructor
